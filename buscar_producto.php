dcf6292910c49216b58d13bb1b26247ff5093843
Carlos Salazar Moreira <56647144+carfesal@users.noreply.github.com>
2021-09-13 08:01:48 -0500

    Cambios

---
+++ buscar_producto.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('connection.php');
+
+
+    $producto = $_POST['producto'];
+    $sql = "SELECT * FROM producto WHERE LOWER(nombre) like Lower('%".$producto."%')";
+    $result = mysqli_query($conn,$sql);
+    $rows = mysqli_num_rows($result);
+    if($rows > 0){
+        $productos_csv = "";
+        while($fila=mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $productos .= $fila['nombre'].",".$fila['precio']."\n";     
+        }
+        echo $productos_csv;            
+    }else{
+        echo "No se encontraron productos";
+    }
+
+            
+?>
